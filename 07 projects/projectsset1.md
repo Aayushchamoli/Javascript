d59f417828b4773edaadfa0441b6ad1c4757d897
Aayushchamoli <chamoli2004@gmail.com>
2024-09-06 11:28:14 +0530

    Projects

---
+++ 07 projects/projectsset1.md
@@ -0,0 +1,16 @@
+#projects related to DOM
+
+
+##projects links
+[click here]
+
+
+#solution code
+
+##projects 1
+
+
+
+
+
+
---
+++ project 4/code.js
@@ -0,0 +1,19 @@
+const randomnumber=(parseInt(Math.random() * 100 + 1));
+
+const submit=document.querySelector('#subt');
+const user=document.querySelector('#guessField');
+const guessSlot=document.querySelector('.guesses');
+const remaining =document.querySelector('.lastResult');
+const low=document.querySelector('#subt')
+
+
+
+
+
+
+
+
+
+
+
+
